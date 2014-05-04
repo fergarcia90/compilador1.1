864fe2d37e279c4893acd5b4ac997fb382a34804
Fernando Garcia <fer.garcia90@me.com>
2014-05-04 15:20:50 -0500

    52 casos

--- Sintactico3/Sintactico.java
+++ Sintactico3/Sintactico.java
@@ -104,7 +104,171 @@ public class Sintactico {
 				switch(regla+1){
 				
 				case 1:
-					new R1(pila);
+					nodo=new R1(pila);
+					break;
+					
+				case 3:
+					nodo=new R3(pila);
+					break;
+					
+				case 4:
+					nodo=new R4(pila);
+					break;
+					
+				case 5:
+					nodo=new R5(pila);
+					break;
+					
+				case 6:
+					nodo=new R6(pila);
+					break;
+					
+				case 8:
+					nodo=new R8(pila);
+					break;
+					
+				case 9:
+					nodo=new R9(pila);
+					break;
+					
+				case 11:
+					nodo=new R11(pila);
+					break;
+					
+				case 13:
+					nodo=new R13(pila);
+					break;
+					
+				case 14:
+					nodo=new R14(pila);
+					break;
+					
+				case 16:
+					nodo=new R16(pila);
+					break;
+					
+				case 17:
+					nodo=new R17(pila);
+					break;
+					
+				case 18:
+					nodo=new R18(pila);
+					break;
+					
+				case 20:
+					nodo=new R20(pila);
+					break;
+					
+				case 21:
+					nodo=new R21(pila);
+					break;
+					
+				case 22:
+					nodo=new R22(pila);
+					break;
+					
+				case 23:
+					nodo=new R23(pila);
+					break;
+					
+				case 24:
+					nodo=new R24(pila);
+					break;
+					
+				case 25:
+					nodo=new R25(pila);
+					break;
+					
+				case 27:
+					nodo=new R27(pila);
+					break;
+					
+				case 28:
+					nodo=new R28(pila);
+					break;
+					
+				case 30:
+					nodo=new R30(pila);
+					break;
+					
+				case 32:
+					nodo=new R32(pila);
+					break;
+					
+				case 34:
+					nodo=new R34(pila);
+					break;
+					
+				case 35:
+					nodo=new R35(pila);
+					break;
+					
+				case 36:
+					nodo=new R36(pila);
+					break;
+					
+				case 37:
+					nodo=new R37(pila);
+					break;
+					
+				case 38:
+					nodo=new R38(pila);
+					break;
+					
+				case 39:
+					nodo=new R39(pila);
+					break;
+					
+				case 40:
+					nodo=new R40(pila);
+					break;
+					
+				case 41:
+					nodo=new R41(pila);
+					break;
+					
+				case 42:
+					nodo=new R42(pila);
+					break;
+					
+				case 43:
+					nodo=new R43(pila);
+					break;
+					
+				case 44:
+					nodo=new R44(pila);
+					break;
+					
+				case 45:
+					nodo=new R45(pila);
+					break;
+					
+				case 46:
+					nodo=new R46(pila);
+					break;
+					
+				case 47:
+					nodo=new R47(pila);
+					break;
+					
+				case 48:
+					nodo=new R48(pila);
+					break;
+					
+				case 49:
+					nodo=new R49(pila);
+					break;
+					
+				case 50:
+					nodo=new R50(pila);
+					break;
+					
+				case 51:
+					nodo=new R51(pila);
+					break;
+					
+				case 52:
+					nodo=new R52(pila);
 					break;
 				
 				default:
